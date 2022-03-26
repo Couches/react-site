ec80cee424bae12a13e510796031e2f11eb04a28
Couches <robertgdel@gmail.com>
2022-03-26 03:09:12 -0700

    minor updates

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,14 @@
 import React from "react";
+import "./home.css";
 
 function Home() {
   return (
-    <></>
+    <div className="home">
+      <div className="title">
+        <h1>Welcome</h1>
+        <h3>to my page</h3>
+      </div>
+    </div>
   );
 }
 
